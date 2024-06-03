609a196635b0884c06fad4d07bf6cfb9b7143ebd
kholoudjuhany <juhanykholoud1@gmail.com>
2024-06-03 22:52:40 +0300

    js

---
+++ equations.js
@@ -0,0 +1,67 @@
+let cash, liabilites, cashFlowRatio;  
+cash = 1000;        
+liabilites = 500;        
+cashFlowRatio = cash / liabilites; 
+console.log(cashFlowRatio);
+  
+// equation 2
+let revenue, expenses, netIncome;  
+revenue = 1000;        
+expenses = 500;        
+netIncome = revenue - expenses;
+console.log(netIncome);
+
+// equation 3
+let liable, equity, totalAssets;  
+liable = 1000;        
+equity = 500;        
+totalAssets = liable + equity
+console.log(totalAssets);
+
+// equation 4
+let profit, sales, ni;  
+profit = 1000;        
+sales = 500;        
+ni = profit * sales;
+console.log(ni);
+
+// equation 5
+let a = 7;
+let b = 9;
+let c = 2;
+let average = (a+ b +c) / 3;
+console.log(average);
+
+// equation 6
+let price = 150;
+let discount = 0.3;
+let dis = price - (price * discount);
+console.log(dis);
+
+// equation 7 
+let age = 20;
+let x;
+
+if (age > 18 && age < 30) 
+    x = 1;
+else
+    x =0;
+console.log(Boolean(x));
+
+// equation 8 
+let d = 2;
+let z = d ** 3; 
+console.log(z);
+
+// equation 9 
+let n1 = 10;
+let n2 = 4;
+let n3 = n1 % n2;
+
+console.log(n3);
+
+    
+
+
+
+  
