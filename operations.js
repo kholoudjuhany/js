524fe4323b1e2fffac38a5b7f580132776d96c07
kholoudjuhany <juhanykholoud1@gmail.com>
2024-06-04 09:49:42 +0300

    javascript

---
+++ operations.js
@@ -0,0 +1,33 @@
+var a = 10;
+var b = 5;
+
+// Arthimetic
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+// Comparison
+console.log(a > b);
+console.log(a < b);
+console.log(a == b);
+console.log(a != b);
+console.log(a >= b);
+console.log(a <= b);
+
+// Logical
+var x = true;
+var y = false;
+console.log(x & y);
+console.log(x || y);
+console.log(!x & !y);
+
+// Assignment
+console.log(a += 5);
+console.log(a -= 5);
+console.log(a *= 5);
+
+
+
+
